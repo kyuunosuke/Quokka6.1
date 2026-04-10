ee8df385f163f702d08601ecd6bfa905858538a6
kyuunosuke <kyuunosuke.t@gmail.com>
2026-04-10 04:43:10 +0000

    1004.1

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.quokkamole.com',
+  generateRobotsTxt: true, // also creates robots.txt automatically
+  sitemapSize: 7000,
+}
